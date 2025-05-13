1c19bdc9ed816ca1045f77b9042099f3281a3297
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2025-05-13 10:59:23 +1000

    updated subnet

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "vpc" {
 
   azs             = ["ap-southeast-2a", "ap-southeast-2b"]
   private_subnets = ["10.0.0.0/28", "10.0.0.16/28"]
-  public_subnets  = ["10.0.0.32/28", "10.0.0.648/28"]
+  public_subnets  = ["10.0.0.32/28", "10.0.0.64/28"]
   create_igw      = true  
   tags = {
     Terraform = "true"
