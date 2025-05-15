ad020ce33dae518a92dd040626494d4026032c83
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2025-05-15 13:26:19 +1000

    adding asg

--- main.tf
+++ main.tf
@@ -101,3 +101,64 @@ resource "aws_launch_template" "ec2_template" {
 }
 
 
+
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+
+  # Autoscaling group
+  name = "example-asg"
+
+  min_size                  = 0
+  max_size                  = 1
+  desired_capacity          = 1
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = [modue.vpc.vpc_id]
+
+
+  instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+      checkpoint_delay       = 600
+      checkpoint_percentages = [35, 70, 100]
+      instance_warmup        = 300
+      min_healthy_percentage = 50
+      max_healthy_percentage = 100
+    }
+    triggers = ["tag"]
+  }
+
+  # Launch template
+  launch_template_name        = "new-template"
+  launch_template_description = "Launch template example"
+  update_default_version      = true
+
+  image_id          = "data.aws_ami.id"
+  instance_type     = "t3.micro"
+  enable_monitoring = true
+
+  block_device_mappings = [
+    {
+      # Root volume
+      device_name = "/dev/xvda"
+      no_device   = 0
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 20
+        volume_type           = "gp2"
+      }
+    }
+  ]
+  cpu_options = {
+    core_count       = 1
+    threads_per_core = 1
+  }
+
+ 
+  tags = {
+    Environment = "dev"
+    Project     = "megasecret"
+  }
+}
+
