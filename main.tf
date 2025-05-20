75a3fa3c54d6eb704567eeec8ef47302c11905b9
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2025-05-20 13:39:32 +1000

    create asg

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 module "launch_template" {
   source = "./launch-template"
 }
+
+
+module "create_ASG" {
+  source = "./terraform-aws-autoscaling-master/terraform-aws-autoscaling-master/"
+  create_launch_template = false
+  launch_template_id        = module.launch_template.launch_template_id
+}
