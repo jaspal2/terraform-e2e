14a975d98fcdf4391305a940a1f9710168d5cf7e
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2025-05-20 14:01:15 +1000

    output

--- launch-template/outputs.tf
+++ launch-template/outputs.tf
@@ -1,3 +1,9 @@
 output "launch_template_id" {
     value = aws_launch_template.terraform_template.id
 }
+
+# Module 2 (Launch Template) outputs.tf
+output "vpc" {
+  value       = module.vpc
+  description = "All outputs from the VPC module"
+}
