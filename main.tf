88edab257a4ab0946f2ddd2dafd6b4f996eb34f3
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2025-05-20 13:56:43 +1000

    asg

--- main.tf
+++ main.tf
@@ -8,4 +8,22 @@ module "create_ASG" {
   name = "Terrafomr_asg"
   create_launch_template = false
   launch_template_id        = module.launch_template.launch_template_id
+  min_size = 0
+  max_size = 0
+  desired_capacity          = 1
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = [module.launch_template.vpc.public_subnets]
+
+    instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+      checkpoint_delay       = 600
+      checkpoint_percentages = [35, 70, 100]
+      instance_warmup        = 300
+      min_healthy_percentage = 50
+      max_healthy_percentage = 100
+    }
+    triggers = ["tag"]
+  }
 }
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,3 +1,7 @@
 output "security_group_id" {
   value = module.web_server_sg.security_group_id
 }
+
+output "public_subnets" {
+  value = moduel.vpc.public_subnets
+}
