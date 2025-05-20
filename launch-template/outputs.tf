f37fd0a49a8def75e514e0f91893d7e98f008eeb
jaspal2 <47457335+jaspal2@users.noreply.github.com>
2025-05-20 14:03:47 +1000

    launch template o/p

--- launch-template/outputs.tf
+++ launch-template/outputs.tf
@@ -2,8 +2,7 @@ output "launch_template_id" {
     value = aws_launch_template.terraform_template.id
 }
 
-# Module 2 (Launch Template) outputs.tf
-output "vpc" {
-  value       = module.vpc
-  description = "All outputs from the VPC module"
+
+output "public_subnets"{
+    value = module.vpc.public_subnets
 }
